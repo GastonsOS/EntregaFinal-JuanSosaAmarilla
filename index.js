17d15fc9ab5f244e300c00eecb4df7f0e9004356
GastonsOS <gaston25sosa@gmail.com>
2023-08-01 20:57:53 -0300

    agregue filtro

--- index.js
+++ index.js
@@ -1,72 +1,58 @@
-//==============Variables=================//
-let nombre = ""
-let txtPeso
-let txtAltura
+class Persona {
+    constructor(nombre, edad, peso, altura, imc) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.peso = peso;
+    this.altura = altura;
+    this.imc = imc;
+    }
+}
 
-let peso = 0
-let altura = 0
-let imc = 0
-let btnCalcular = 0
+let datosPersonasIngreso = [];
+console.log(Persona)
 
-//Bucle//
+  //==============Variables=================//
+let nombre;
+let txtPeso;
+let txtAltura;
+
+let peso = 0;
+let altura = 0;
+let imc = 0;
+let btnCalcular = 0;
+
+  //Bucle//
 
 do {
-    nombre = prompt("¿Podrías escribir tu nombre?")
-    
-} while (!isNaN(nombre) || "")
-    alert("Bienvenido " + nombre)
+    nombre = prompt("¿Podrías escribir tu nombre?");
 
-//Función de peso y altura//
+    //aca escribi toda la logica de carga de datos
 
-function clickBtnCalcular() {
-    txtPeso = document.getElementById('txtPeso')
-    peso = txtPeso.value
+    if (nombre !== "ESC") {
+    alert("Bienvenido " + nombre);
 
-    txtAltura = document.getElementById('txtAltura')
-    altura = txtAltura.value
-
-//Calculadora//
-imc = peso / (altura * altura)
-    alert(nombre + " tú IMC es: " + Math.round(imc))
-
-}
-
-let edad = prompt("Ingresa tú edad");
-
-if(edad == "") {
-    alert("No ingresaste ninguna edad.")
-}
-
-else {
-    alert("Edad Ingresada" + edad)
-}
-
-//Función de botón//
-function iniciar() {
-    btnCalcular = document.getElementById("btnCalcular")
-    btnCalcular.addEventListener("click", clickBtnCalcular)
-}
-
-window.onload = iniciar
-
-class Persona {
-    contructor (nombre, edad, peso, altura) {
-        this.nombre = nombre;
-        this.edad = edad;
-        this.peso = peso;
-        this.altura = altura;
+    let edad = prompt("Ingresa tú edad");
+    if (edad === "") {
+        alert("No ingresaste ninguna edad.");
+    } else {
+        alert("Edad Ingresada: " + edad);
     }
 
-hablar () {
-    console.log("HOLA" + this.nombre) ;
+    peso = parseFloat(prompt("Ingresa tú peso"));
+
+    altura = parseFloat(prompt("Ingresa tú altura"));
+
+    imc = peso / (altura * altura);
+    alert(nombre + " tú IMC es: " + Math.round(imc));
+    
+    let persona = new Persona(nombre, edad, peso, altura, imc);
+    datosPersonasIngreso.push(persona);
 }
 
-}
+} while (nombre !== "ESC");
 
-//Variables 
+console.log(datosPersonasIngreso);
 
-const datos = {
-    ctxtPeso : document.getElementById('txtPeso') ,
-    ctxtAltura : document.getElementById('txtAltura') ,
-    cbtnCalcular : document.getElementById("btnCalcular")
-}
+datosPersonasIngreso.filter (persona =>{
+    console.log(persona.nombre);
+})
